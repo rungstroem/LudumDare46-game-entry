ebbe5daf0c3f0a0bdad8d151c79a679359b81836
Kenneth Rungstrøm Larsen <rungstroem@hotmail.com>
2020-04-18 21:11:03 +0200

    Working on healthbar in seperate struct

--- spaceInvaders.cpp
+++ spaceInvaders.cpp
@@ -50,6 +50,43 @@ Uint32 AlienForm[20][20] = {	{0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0},
 				{1,1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,1},
 				{1,1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,1}	};
 
+struct healthBar{
+	Uint32 health[5][20] = 	{	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+					{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+					{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+					{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+					{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}	};
+	void reduceHealth(){
+		for(int i = 0; i<20; i++){
+			if(health[0][1] == 0){
+				break;
+			}
+			if(health[0][18] == 1){
+				health[0][18] = 0, health[0][19] = 0;
+				health[1][18] = 0, health[1][19] = 0;
+				health[2][18] = 0, health[2][19] = 0;
+				health[3][18] = 0, health[3][19] = 0;
+				health[4][18] = 0, health[4][19] = 0;
+				break;
+			}
+			if(health[0][i] == 0){
+				health[0][i-1] = 0, health[0][i-2] = 0;
+				health[1][i-1] = 0, health[1][i-2] = 0;
+				health[2][i-1] = 0, health[2][i-2] = 0;
+				health[3][i-1] = 0, health[3][i-2] = 0;
+				health[4][i-1] = 0, health[4][i-2] = 0;
+				break;
+			}
+		}
+	}
+};
+
+struct alien{
+	Uint32 form[][] = AlienForm;
+	healthBar h;
+	Uint32 health[][] = h.health;
+};
+
 using namespace std;
 int main(int argc, char* args[]){
 	Uint32 shipForm1D[20*20];
@@ -61,8 +98,8 @@ int main(int argc, char* args[]){
 		}
 	}
 	for(int i = 0;i<20*20;i++){
-		if(shipForm1D[i] == 1) shipForm1D[i] == 0xFFFFFFF0;
-		if(AlienForm1D[i] == 1) AlienForm1D[i] == 0xFFFFFFF0;
+		if(shipForm1D[i] == 1) shipForm1D[i] == 0xFFFFFFFF;
+		if(AlienForm1D[i] == 1) AlienForm1D[i] == 0xFFFFFFFF;
 	}
 
 	//Initialize window and render
@@ -87,16 +124,16 @@ int main(int argc, char* args[]){
 		cout << "Window or renderer could not be created\n";
 		quit = true;
 	}
-	/*	
-	SDL_Texture *ship = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, 20,20);
-	SDL_UpdateTexture(ship, NULL, shipForm1D, 8*20);
 
-	SDL_Rect rect;
-	rect.x = 0, rect.y = 0, rect.w = 20, rect.h = 20;
-	*/
+	//Create number of aliens
+	for(int i = 0; i < screenWidth / 25; i++){
+		if(i>100) i = 100;
+		Alien + "i"
+	}
 
-	int playerPosX = screenHeight -20;
-	int playerPosY = screenWidth/2;
+
+	int playerPosX = screenWidth / 2 -10;
+	int playerPosY = screenHeight - 20;
 	
 	//Game-loop
 	while(!quit){
@@ -108,23 +145,23 @@ int main(int argc, char* args[]){
 			} else if(inputEvent.type = SDL_KEYDOWN){
 				switch (inputEvent.key.keysym.sym){
 					case SDLK_UP:
-						playerPosY--;
+						playerPosY-=10;
 						if(playerPosY<0) playerPosY=0;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_DOWN:
-						playerPosY++;
-						if(playerPosY>screenHeight) playerPosY=screenHeight;
+						playerPosY+=10;
+						if(playerPosY>screenHeight -20) playerPosY=screenHeight -20;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_LEFT:
-						playerPosX--;
+						playerPosX-=10;
 						if(playerPosX<0) playerPosX=0;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_RIGHT:
-						playerPosX++;
-						if(playerPosX>screenWidth) playerPosX=screenWidth;
+						playerPosX+=10;
+						if(playerPosX>screenWidth -20) playerPosX=screenWidth -20;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_SPACE:
@@ -134,32 +171,33 @@ int main(int argc, char* args[]){
 				}
 			}
 		}
-		/*	
+		//Clear screen
 		SDL_SetRenderDrawColor(render, 0,0,0,0);
 		SDL_RenderClear(render);
-		//SDL_RenderCopy(render, ship, NULL, NULL);
-		SDL_SetRenderDrawColor(render, 255,255,255,100);
-		SDL_RenderDrawRect(render, &rect);
-		*/
-
+		
+		//Draw spaceship
 		SDL_SetRenderDrawColor(render, 255,255,255,255);
-		//SDL_RenderDrawPoint(render, x, y);
-		for(int i = playerPosX; i<playerPosX+20; i++){
-			for(int j = playerPosY; j<playerPosY+20; j++){
+		for(int i = playerPosX; i < playerPosX + 20; i++){
+			for(int j = playerPosY; j < playerPosY + 20; j++){
+				
 				if(shipForm[j-playerPosY][i-playerPosX] == 1){
-					if(i>screenHeight) i=screenHeight-1;
-					if(j>screenWidth) j=screenWidth-1;
 					SDL_RenderDrawPoint(render, i,j);
 				}
 			}
 		}
-		//End of game-loop Draws to screen
+	
+		//Draw aliens
+		for(int i = 0; i < screenWidth / 25; i++){
+			
+		}
+
+
+		//Update screen - end of game-loop
 		SDL_RenderPresent(render);
 
 	}
 
 	//Clean-up
-	//SDL_DestroyTexture(ship);
 	SDL_DestroyRenderer(render);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
