af67f7239040898dd04127d3d05b32af671ab65d
Kenneth Rungstrøm Larsen <rungstroem@hotmail.com>
2020-04-18 19:08:21 +0200

    With rect

--- spaceInvaders.cpp
+++ spaceInvaders.cpp
@@ -8,16 +8,67 @@ const int screenHeight = 600;
 bool quit = false;
 //Uint32 *pixels;
 
+Uint32 shipForm[20][20] = {	{0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0},
+				{0,0,1,1,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0},
+				{0,0,1,1,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0},
+				{0,0,1,1,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0},
+				{0,0,1,1,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0},
+				{0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0},
+				{0,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0},
+				{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+				{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+				{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+				{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{0,0,0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0}	};
+
+Uint32 AlienForm[20][20] = {	{0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0},
+				{0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0},
+				{0,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,0},
+				{0,0,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,0,0},
+				{0,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,0},
+				{1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1},
+				{1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1},
+				{1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1},
+				{1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,1},
+				{1,1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,1},
+				{1,1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,1}	};
+
 using namespace std;
 int main(int argc, char* args[]){
-	
-	//Initialize window, screen realestate and render
-	SDL_Window *window = NULL;
-	SDL_Surface *screenSurface = NULL;
-	SDL_Surface *backBuffer = NULL;
-	SDL_Renderer *render = NULL;
-	//SDL_Texture *texture = NULL;
+	Uint32 shipForm1D[20*20];
+	Uint32 AlienForm1D[20*20];
+	for(int i = 0;i<20;i++){
+		for(int j = 0;j<20;j++){
+			shipForm1D[i*20+j] = shipForm[i][j];
+			AlienForm1D[i*20+j] = AlienForm[i][j];
+		}
+	}
+	for(int i = 0;i<20*20;i++){
+		if(shipForm1D[i] == 1) shipForm1D[i] == 0xFFFFFFF0;
+		if(AlienForm1D[i] == 1) AlienForm1D[i] == 0xFFFFFFF0;
+	}
 
+	//Initialize window and render
+	SDL_Window *window = NULL;
+	SDL_Renderer *render = NULL;
+	
 	//Setup eventhandler
 	SDL_Event inputEvent;
 
@@ -28,37 +79,28 @@ int main(int argc, char* args[]){
 	} else{
 		window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
 		render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		//texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, screenWidth, screenHeight);	
-		//SDL_CreateWindowAndRenderer(screenWidth, screenHeight, 0, &window, &render);
-		//window = SDL_CreateWindowAndRenderer("spaceInvaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
 		quit = false;
 	}
 	
 	//Check whether the window has been created
-	if( window == NULL || render == NULL || texture == NULL){
+	if( window == NULL || render == NULL ){
 		cout << "Window or renderer could not be created\n";
 		quit = true;
-	} else{
-		screenSurface = SDL_GetWindowSurface(window);
-		//pixels = new Uint32[screenWidth*screenHeight];
-		//memset(pixels, 0, screenWidth*screenHeight*sizeof(Uint32));
-
-		quit = false;
-
-		//SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-		//SDL_UpdateWindowSurface(window); //Updates screen with content
-		//SDL_Delay(2000);
 	}
+	/*	
+	SDL_Texture *ship = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, 20,20);
+	SDL_UpdateTexture(ship, NULL, shipForm1D, 8*20);
 
-	int x = 100;
-	int y = 100;
+	SDL_Rect rect;
+	rect.x = 0, rect.y = 0, rect.w = 20, rect.h = 20;
+	*/
+
+	int playerPosX = screenHeight -20;
+	int playerPosY = screenWidth/2;
 	
 	//Game-loop
 	while(!quit){
-		SDL_SetRenderDrawColor(render, 0,0,0,0);
-
-		//SDL_UpdateTexture(texture, NULL, pixels, screenWidth*sizeof(Uint32));
-
+		
 		//Input event handling
 		while(SDL_PollEvent(&inputEvent) != 0){
 			if(inputEvent.type == SDL_QUIT || inputEvent.key.keysym.sym == SDLK_ESCAPE){
@@ -66,23 +108,23 @@ int main(int argc, char* args[]){
 			} else if(inputEvent.type = SDL_KEYDOWN){
 				switch (inputEvent.key.keysym.sym){
 					case SDLK_UP:
-						y--;
-						if(y<0) y=0;
+						playerPosY--;
+						if(playerPosY<0) playerPosY=0;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_DOWN:
-						y++;
-						if(y>screenHeight) y=screenHeight;
+						playerPosY++;
+						if(playerPosY>screenHeight) playerPosY=screenHeight;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_LEFT:
-						x--;
-						if(x<0) x=0;
+						playerPosX--;
+						if(playerPosX<0) playerPosX=0;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_RIGHT:
-						x++;
-						if(x>screenWidth) x=screenWidth;
+						playerPosX++;
+						if(playerPosX>screenWidth) playerPosX=screenWidth;
 						//Do stuff with keyinputs
 						break;
 					case SDLK_SPACE:
@@ -92,27 +134,32 @@ int main(int argc, char* args[]){
 				}
 			}
 		}
+		/*	
+		SDL_SetRenderDrawColor(render, 0,0,0,0);
 		SDL_RenderClear(render);
-		//SDL_RenderCopy(render, texture, 0,0);
-		//SDL_RenderPresent(render);	
-		SDL_RenderDrawPoint(render, x, y);
+		//SDL_RenderCopy(render, ship, NULL, NULL);
+		SDL_SetRenderDrawColor(render, 255,255,255,100);
+		SDL_RenderDrawRect(render, &rect);
+		*/
+
+		SDL_SetRenderDrawColor(render, 255,255,255,255);
+		//SDL_RenderDrawPoint(render, x, y);
+		for(int i = playerPosX; i<playerPosX+20; i++){
+			for(int j = playerPosY; j<playerPosY+20; j++){
+				if(shipForm[j-playerPosY][i-playerPosX] == 1){
+					if(i>screenHeight) i=screenHeight-1;
+					if(j>screenWidth) j=screenWidth-1;
+					SDL_RenderDrawPoint(render, i,j);
+				}
+			}
+		}
+		//End of game-loop Draws to screen
 		SDL_RenderPresent(render);
 
-		//End of game-loop Draws to screen
-		//SDL_RenderPresent(render);
-
-		
-		/* 
-		 * Something to figure out later on
-		 * //End of game-loop - Draws to screen
-		 * SDL_BlitSurface(backBuffer, NULL, screenSurface, NULL);	//Copy the backBuffer to the screenSurface
-		 * SDL_UpdateWindowSurface(window);	//Updates the window with the screenSurface
-		*/
 	}
 
 	//Clean-up
-	delete[] pixels;
-	SDL_DestroyTexture(texture);
+	//SDL_DestroyTexture(ship);
 	SDL_DestroyRenderer(render);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
